32f417001c7c507616c1d2e271d48a4c1c721df5
Luisa Mazzi <luisa.c.mazzi@gmail.com>
2023-01-25 11:29:01 -0300

    results page styled

--- lib/constants.dart
+++ lib/constants.dart
@@ -42,8 +42,25 @@ const kCalculateBttnStyle = TextStyle(
     fontWeight: FontWeight.w900,
 );
 
+/* Results page style */
+
 const kTitleTextStyle = TextStyle(
     color: Colors.white,
     fontSize: 50.0,
     fontWeight: FontWeight.bold,
+);
+
+const kWeightLabelTextStyle = TextStyle(
+    color: Color(0xFF24D876),
+    fontSize: 22.0,
+    fontWeight: FontWeight.bold,
+);
+
+const kBMITextStyle = TextStyle(
+    fontSize: 100.0,
+    fontWeight: FontWeight.bold
+);
+
+const kBodyTextStyle = TextStyle(
+    fontSize: 22.0,
 );
--- lib/results_page.dart
+++ lib/results_page.dart
@@ -1,7 +1,24 @@
+import 'package:bmi_calculator/reusable_card.dart';
 import 'package:flutter/material.dart';
 import 'constants.dart';
 
 class ResultsPage extends StatelessWidget {
+  Map<int, String> labels = {
+    0: 'underweight',
+    1: 'ideal',
+    2: 'overweight'
+  };
+
+  Map<int, String> interpretations = {
+    0: 'You have a lower than normal body weight.',
+    1: 'Your body weight is perfect.',
+    2: 'You hav a higher than normal body weight. Try to exercise more.'
+  };
+
+
+
+  int result = 0;
+  double calculatedBMI = 0.0;
 
   @override
   Widget build(BuildContext context) {
@@ -14,7 +31,22 @@ class ResultsPage extends StatelessWidget {
         children: <Widget>[
           Expanded(
             child: Container(
-              child: Text('Your Result', style: kTitleTextStyle,),
+              child: Text('Your Result', style: kTitleTextStyle, textAlign: TextAlign.center,),
+            ),
+          ),
+          Expanded(
+            flex: 5,
+            child: ReusableCard(
+              color: kActiveCardColor,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget> [
+                  Text(labels[result], style: kWeightLabelTextStyle, textAlign: TextAlign.center,),
+                  Text(calculatedBMI.toString(), style: kBMITextStyle, textAlign: TextAlign.center,),
+                  Text(interpretations[result], style: kBodyTextStyle, textAlign: TextAlign.center,),
+                ],
+              ),
             ),
           ),
         ],
